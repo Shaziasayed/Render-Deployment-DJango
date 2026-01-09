6bcc0871b372b0acf1946e8f3200775022c1ad99
Shaziasayed <sayedshazia070@gmail.com>
2026-01-09 23:20:50 +0530

    Remove api references from tutorial

--- backened/wsgi.py
+++ backened/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'api.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backened.settings'
+settings_module = 'backened.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'backened.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 application = get_wsgi_application()
