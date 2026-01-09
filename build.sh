5d5c88b86738c0ca84e7e15ced6329b17d4413b4
Shaziasayed <sayedshazia070@gmail.com>
2026-01-09 23:49:19 +0530

    removed  our superuser

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[$CREATE_SUPERUSER]];
-then
-    python manage.py createsuperuser --noinput 
-fi
+# if [[$CREATE_SUPERUSER]];
+# then
+#     python manage.py createsuperuser --noinput 
+# fi
