c803f8a6ecc1202075e5bf2c70e15f1ccc519861
Shaziasayed <sayedshazia070@gmail.com>
2026-01-09 23:40:15 +0530

    create our superuser

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --noinput 
+fi
