74840a478f758729a3a6f8c5e30b2ff046b1ba21
Shaziasayed <sayedshazia070@gmail.com>
2026-01-12 14:54:24 +0530

    core headers

--- backened/deployment_settings.py
+++ backened/deployment_settings.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://render-deploy-react-code-pyy3.onrender.com']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
